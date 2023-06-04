7eed75967c5c3263ced1cecc50755d7de7c48304
Aron Rohr <luiggi33@web.de>
2023-06-04 11:11:14 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# SFTP Upload GitHub Action
+
+This GitHub Action allows you to automate the upload of changed and new files to an SFTP server using lftp.
+
+## Usage
+
+To use this action, you need to include it as a step in your GitHub Actions workflow. Here's an example:
+
+```yaml
+name: SFTP Upload
+
+on:
+  push:
+    branches:
+      - main
+
+jobs:
+  upload:
+    runs-on: ubuntu-latest
+
+    steps:
+    - name: Check Out Code
+      uses: actions/checkout@v2
+
+    - name: SFTP Upload
+      uses: luiggi33/sftp-upload-action@v1
+      with:
+        sftp-host: ${{ secrets.SFTP_HOST }}
+        sftp-user: ${{ secrets.SFTP_USER }}
+        ssh-private-key: ${{ secrets.SSH_PRIVATE_KEY }}
+        remote-path: path/on/remote/server
---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Set variables
+SFTP_HOST="$1"
+SFTP_USER="$2"
+SSH_PRIVATE_KEY="$3"
+REMOTE_PATH="$4"
+
+# Create SSH directory and set permissions
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+# Write SSH private key to a file
+echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+# Set strict SSH key checking
+echo "StrictHostKeyChecking no" > ~/.ssh/config
+
+# Get the list of changed and new files
+git diff-tree --no-commit-id --name-only -r ${{ github.sha }} > changed_files.txt
+
+# Upload changed and new files via SFTP
+lftp -c "set ftp:ssl-allow no; open -u $SFTP_USER -e 'mirror -R --delete --only-newer --exclude-glob .git/ --exclude-glob .github/ --exclude-glob *.sh -P1 --parallel=10 -x changed_files.txt $REMOTE_PATH' $SFTP_HOST"
+
+# Clean up
+rm changed_files.txt
